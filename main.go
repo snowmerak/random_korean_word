c3da94ebef6cfd0d5d88d211b06e74a9a59c90ff
WooSung <wsjung@connectfic.co.kr>
2022-07-07 13:30:20 +0900

    Update GetLength function

--- main.go
+++ main.go
@@ -11,3 +11,18 @@ func Get() string {
 	m := binary.BigEndian.Uint64(b[:])
 	return data[m%uint64(len(data))]
 }
+
+func GetLength(length int) string {
+	result := ""
+	for i := 1; i <= length; i++ {
+		b := [8]byte{}
+		rand.Read(b[:])
+		m := binary.BigEndian.Uint64(b[:])
+		if i != 1 {
+			result = result + " " + data[m%uint64(len(data))]
+		} else {
+			result = data[m%uint64(len(data))]
+		}
+	}
+	return result
+}
